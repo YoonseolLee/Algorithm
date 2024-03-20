54cb9ef7352f5969e4095719362eee4b75c0190e
Yoonseol Lee <107620081+YoonseolLee@users.noreply.github.com>
2024-03-20 13:51:59 +0900

    [Bronze II] Title: 피보나치 수, Time: 120 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,6 +1,5 @@
 
 import java.io.*;
-
 class Main {
     static long[] fibonacciCache;
     public static void main(String[] args) throws IOException {
@@ -18,44 +17,44 @@ class Main {
     }
 
     // 1. 재귀 함수 구현
-    static long fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        if (fibonacciCache[n] != 0) {
-            return fibonacciCache[n];
-        } else {
-            long fibonacciResult = fibonacci(n - 1) + fibonacci(n - 2);
-            fibonacciCache[n] = fibonacciResult;
-            return fibonacciResult;
-        }
-    }
-    
-    
-//    // 2. 반복문
 //    static long fibonacci(int n) {
 //        if (n <= 1) {
 //            return n;
-//            // base case
 //        }
-//
-//        fibonacciCache[0] = 0;
-//        fibonacciCache[1] = 1;
-//
-//        /**
-//         * fibonacciCache[0],[1]은 이미 값이 있기 때문에 [2]부터 n까지 계산한다.
-//         * Example) n = 4인경우,
-//         * 1. fibonacciCache[2] = fibonacciCache[1] + fibonacciCache[0] = 1 + 0 = 1
-//         * 2. fibonacciCache[3] = fibonacciCache[2] + fibonacciCache[1] = 1 + 1 = 2
-//         * 3. fibonacciCache[4] = fibonacciCache[3] + fibonacciCache[2] = 2 + 1 = 3
-//         * 결과는 3이다.
-//         */
-//        for (int i = 2; i <= n; i++) {
-//            fibonacciCache[i] = fibonacciCache[i-1] + fibonacciCache[i-2];
+//        if (fibonacciCache[n] != 0) {
+//            return fibonacciCache[n];
+//        } else {
+//            long fibonacciResult = fibonacci(n - 1) + fibonacci(n - 2);
+//            fibonacciCache[n] = fibonacciResult;
+//            return fibonacciResult;
 //        }
-//        return fibonacciCache[n];
-//        // recursive case
 //    }
+    
+    
+    // 2. 반복문
+    static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+            // base case
+        }
+
+        fibonacciCache[0] = 0;
+        fibonacciCache[1] = 1;
+
+        /**
+         * fibonacciCache[0],[1]은 이미 값이 있기 때문에 [2]부터 n까지 계산한다.
+         * Example) n = 4인경우,
+         * 1. fibonacciCache[2] = fibonacciCache[1] + fibonacciCache[0] = 1 + 0 = 1
+         * 2. fibonacciCache[3] = fibonacciCache[2] + fibonacciCache[1] = 1 + 1 = 2
+         * 3. fibonacciCache[4] = fibonacciCache[3] + fibonacciCache[2] = 2 + 1 = 3
+         * 결과는 3이다.
+         */
+        for (int i = 2; i <= n; i++) {
+            fibonacciCache[i] = fibonacciCache[i-1] + fibonacciCache[i-2];
+        }
+        return fibonacciCache[n];
+        // recursive case
+    }
 }
 
 
