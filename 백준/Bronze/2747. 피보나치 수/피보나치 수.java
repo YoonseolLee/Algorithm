20cdc414c705f1664f0dfa9a44526e4cc7c635cc
Yoonseol Lee <107620081+YoonseolLee@users.noreply.github.com>
2024-03-20 13:51:24 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 
 class Main {
@@ -17,7 +18,6 @@ class Main {
     }
 
     // 1. 재귀 함수 구현
-    /*
     static long fibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -30,32 +30,32 @@ class Main {
             return fibonacciResult;
         }
     }
-    */
     
-    // 2. 반복문
-    static long fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-            // base case
-        }
-
-        fibonacciCache[0] = 0;
-        fibonacciCache[1] = 1;
-
-        /**
-         * fibonacciCache[0],[1]은 이미 값이 있기 때문에 [2]부터 n까지 계산한다.
-         * Example) n = 4인경우, 
-         * 1. fibonacciCache[2] = fibonacciCache[1] + fibonacciCache[0] = 1 + 0 = 1
-         * 2. fibonacciCache[3] = fibonacciCache[2] + fibonacciCache[1] = 1 + 1 = 2
-         * 3. fibonacciCache[4] = fibonacciCache[3] + fibonacciCache[2] = 2 + 1 = 3
-         * 결과는 3이다. 
-         */
-        for (int i = 2; i <= n; i++) {
-            fibonacciCache[i] = fibonacciCache[i-1] + fibonacciCache[i-2];
-        }
-        return fibonacciCache[n];
-        // recursive case
-    }
+    
+//    // 2. 반복문
+//    static long fibonacci(int n) {
+//        if (n <= 1) {
+//            return n;
+//            // base case
+//        }
+//
+//        fibonacciCache[0] = 0;
+//        fibonacciCache[1] = 1;
+//
+//        /**
+//         * fibonacciCache[0],[1]은 이미 값이 있기 때문에 [2]부터 n까지 계산한다.
+//         * Example) n = 4인경우,
+//         * 1. fibonacciCache[2] = fibonacciCache[1] + fibonacciCache[0] = 1 + 0 = 1
+//         * 2. fibonacciCache[3] = fibonacciCache[2] + fibonacciCache[1] = 1 + 1 = 2
+//         * 3. fibonacciCache[4] = fibonacciCache[3] + fibonacciCache[2] = 2 + 1 = 3
+//         * 결과는 3이다.
+//         */
+//        for (int i = 2; i <= n; i++) {
+//            fibonacciCache[i] = fibonacciCache[i-1] + fibonacciCache[i-2];
+//        }
+//        return fibonacciCache[n];
+//        // recursive case
+//    }
 }
 
 
